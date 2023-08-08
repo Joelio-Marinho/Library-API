32172fc86ffbda303a06a0292b584a399110b913
Joelio Marinho <joeliomarinho@gmail.com>
2023-08-08 07:24:58 -0300

    adição de codigo

--- src/main/java/com/joelio/libraryapi/service/BookService.java
+++ src/main/java/com/joelio/libraryapi/service/BookService.java
@@ -2,6 +2,9 @@ package com.joelio.libraryapi.service;
 
 import com.joelio.libraryapi.model.Book;
 
+import java.util.Optional;
+
 public interface BookService {
     Book save(Book any);
+    Optional<Book> findByid(Long id);
 }
