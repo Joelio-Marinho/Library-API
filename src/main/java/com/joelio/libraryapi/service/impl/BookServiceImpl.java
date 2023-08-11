06af8cb636c568ed7de9909e025cd5f8428efc4c
Joelio Marinho <joeliomarinho@gmail.com>
2023-08-11 10:09:42 -0300

    adição do metodo findAll no service e os teste para saber se a paginação esta funcionando.

--- src/main/java/com/joelio/libraryapi/service/impl/BookServiceImpl.java
+++ src/main/java/com/joelio/libraryapi/service/impl/BookServiceImpl.java
@@ -5,6 +5,8 @@ import com.joelio.libraryapi.exception.BusinessException;
 import com.joelio.libraryapi.model.Book;
 import com.joelio.libraryapi.repository.BookRepository;
 import com.joelio.libraryapi.service.BookService;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -49,6 +51,11 @@ public class BookServiceImpl implements BookService {
 
     @Override
     public Page<Book> find(Book filter, Pageable pageRequest) {
-        return null;
+        Example<Book> example = Example.of(filter, ExampleMatcher
+                .matching()
+                .withIgnoreCase()
+                .withIgnoreNullValues()
+                .withStringMatcher(ExampleMatcher.StringMatcher.CONTAINING));
+         return repository.findAll(example,pageRequest);
     }
 }
--- src/test/java/com/joelio/libraryapi/service/BookServiceTest.java
+++ src/test/java/com/joelio/libraryapi/service/BookServiceTest.java
@@ -1,6 +1,4 @@
 package com.joelio.libraryapi.service;
-
-import com.joelio.libraryapi.DTO.BookDTO;
 import com.joelio.libraryapi.exception.BusinessException;
 import com.joelio.libraryapi.model.Book;
 import com.joelio.libraryapi.repository.BookRepository;
@@ -12,11 +10,18 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.as;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(SpringExtension.class)
@@ -171,6 +176,28 @@ public class BookServiceTest {
 
         //verificação
         Mockito.verify( repository, Mockito.never()).save(book);
+    }
+
+    @Test
+    @DisplayName("Deve filtrar livros pelas propriedades")
+    public void findBookTest(){
+        Book book = createValidBook();
+
+        PageRequest pageRequest = PageRequest.of(0,10);
+
+        List<Book> lista = Arrays.asList(book);
+
+        Page<Book> page = new PageImpl<Book>(lista, pageRequest ,1 );
+        Mockito.when(repository.findAll(Mockito.any(Example.class), Mockito.any(PageRequest.class)))
+                .thenReturn(page);
+
+
+        Page <Book> result= bookService.find(book, pageRequest);
+
+        assertThat(result.getTotalElements()).isEqualTo(1);
+        assertThat(result.getContent()).isEqualTo(lista);
+        assertThat(result.getPageable().getPageNumber()).isEqualTo(0);
+        assertThat(result.getPageable().getPageSize()).isEqualTo(10);
     }
 
 }
